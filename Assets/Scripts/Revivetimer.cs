e28b1459d6b4a7ca63d2d0a24be02c215d35a266
zNexusz <anuram1231@hotmail.co.uk>
2018-12-03 19:05:57 +0100

    5th

---
+++ Assets/Scripts/Revivetimer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Revivetimer : MonoBehaviour
+{
+
+	public Image Timebar;
+	public float MaxTime = 5f;
+	private float timeLeft;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		Timebar = GetComponent<Image>();
+		timeLeft = MaxTime;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (timeLeft > 0)
+		{
+			timeLeft -= Time.deltaTime;
+			Timebar.fillAmount = timeLeft / MaxTime;
+		}
+		else
+		{
+
+
+		}
+	}
+}
